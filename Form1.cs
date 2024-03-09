9c7c68910cbe9db72f9b914d17e20c3623ee7040
Noah Nassiri <106232024+Noah316-spec@users.noreply.github.com>
2024-03-09 22:00:47 +0100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,86 +13,107 @@ namespace Sodoku
 {
     public partial class Form1 : Form
     {
-        int[] textBoxNumbers21 = new int[82];
+        
         public Form1()
         {
             InitializeComponent();
         }
-
+        void reset()
+        {
+            for (int i = 1; i <= 81; i++)
+            {
+                Control[] buttons = this.Controls.Find("button" + i, true);
+                if (buttons.Length > 0)
+                {
+                    Button button = buttons[0] as Button;
+                    if (button != null)
+                    {
+                        button.Text = "";
+                    }
+                }
+            }
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
+            reset();
             comboBox2.SelectedIndex = 0;
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            int i = 0;
             Random rnd = new Random();
-            for (int i = 1; i <= 81; i++)
+            if(comboBox2.SelectedIndex == 1)
             {
-                // Zufällig auswählen, ob diese TextBox beschrieben werden soll
-                if (rnd.Next(10) > 6) // Ändern Sie die "6" auf eine andere Zahl, um die Wahrscheinlichkeit zu ändern
+                while (i != 10)
                 {
-                    Control[] textBoxes = this.Controls.Find("textBox" + i, true);
-                    if (textBoxes.Length > 0)
+                    int rndbutton = rnd.Next(1, 81);
+                    int rndnum = rnd.Next(1, 9);
+                    Control[] buttons = this.Controls.Find("button" + rndbutton, true);
+                    if (buttons.Length > 0)
                     {
-                        TextBox textBox = textBoxes[0] as TextBox;
-                        if (textBox != null)
+                        Button button = buttons[0] as Button;
+                        if (button != null)
                         {
-                            // Setzen Sie den Text der TextBox auf eine zufällige Zahl zwischen 1 und 9
-                            textBox.Text = rnd.Next(1, 10).ToString();
+                            button.Text = Convert.ToString(rndnum);
                         }
                     }
+                    i++;
                 }
             }
-        }
-
-        bool IsValidSudokuRow(int[] row)
-        {
-            bool[] seen = new bool[10];
-            foreach (int number in row)
+            else
             {
-                if (number < 1 || number > 9 || seen[number])
+                while (i != 7)
                 {
-                    return false;
-                }
-                seen[number] = true;
-            }
-            return true;
-        }
 
+                    int rndbutton = rnd.Next(1, 81);
+                    int rndnum = rnd.Next(1, 9);
+                    Control[] buttons = this.Controls.Find("button" + rndbutton, true);
+                    if (buttons.Length > 0)
+                    {
+                        Button button = buttons[0] as Button;
+                        if (button != null)
+                        {
+                            button.Text = Convert.ToString(rndnum);
+                        }
+                    }
+                    i++;
+                }
+            }
+           
+            
+
+        }
+     
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //array mit werten textboxen
-            int[] textBoxNumbers = { 49, 44, 43, 45, 46, 47, 48, 57, 58, 42, 64, 72, 41, 63, 71, 40, 62, 70, 39, 61, 69, 38, 60, 68, 59, 37, 67, 43, 66, 56, 55, 54, 53, 52, 51, 50, 57, 65, 81, 80, 79, 78, 77, 76, 75, 74, 73 };
-
-            if (comboBox2.SelectedIndex == 0) // inhalt 0
+            int[] wert = { 7, 8, 9, 10, 11, 12, 19, 20, 21, 28, 29, 30, 37, 38, 39, 48, 47, 46, 57, 56, 55, 63, 62, 61, 60, 59, 58, 72, 71, 70, 69, 68, 67, 66, 65, 64, 73, 74, 75, 76, 77, 78, 79, 80, 81 };
+            if (comboBox2.SelectedIndex == 0)
             {
-                // textboxen auf true and false je nach selectedindex 0 oder 1
-                foreach (int number in textBoxNumbers)
+                foreach (int number in wert)
                 {
-                    Control[] textBoxes = this.Controls.Find("textBox" + number, true); 
-                    if (textBoxes.Length > 0)
+                    Control[] buttons = this.Controls.Find("button" + number, true);
+                    if (buttons.Length > 0) // überprüfen ob es das steuerelement gibt 
                     {
-                        TextBox textBox = textBoxes[0] as TextBox;
-                        if (textBox != null)
+                        Button button = buttons[0] as Button;
+                        if (button != null)
                         {
-                            textBox.Visible = false;
+                            button.Visible = false;
                         }
                     }
                 }
-
             }
-            else if (comboBox2.SelectedIndex == 1) // inhalt 1 == 9x9
+            else 
             {
-                foreach (int number in textBoxNumbers)
+                foreach (int number in wert)
                 {
-                    Control[] textBoxes = this.Controls.Find("textBox" + number, true);
-                    if (textBoxes.Length > 0)
+                    Control[] buttons = this.Controls.Find("button" + number, true);
+                    if (buttons.Length > 0)
                     {
-                        TextBox textBox = textBoxes[0] as TextBox;
-                        if (textBox != null)
+                        Button button = buttons[0] as Button;
+                        if (button != null)
                         {
-                            textBox.Visible = true;
+                            button.Visible = true;
                         }
                     }
                 }
@@ -101,7 +122,16 @@ namespace Sodoku
 
         private void btnSolve_Click(object sender, EventArgs e)
         {
-        }
+            
+            
+            
 
+
+        }
+        
+        private void btnres_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
     }
 }
